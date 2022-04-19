c078240c226139ceaf9221d933de71956709a649
Derkach Alexander <derkach2288@gmail.com>
2022-04-19 12:26:54 +0300

    commit2

--- lesson1.rb
+++ lesson1.rb
@@ -1,5 +1,12 @@
-age = 18
+age = 18 # integer .to_i
 puts (age)
 
 age = 24
 puts (age)
+
+name = 'Привет всем' # string .to_s
+puts (name)
+
+happines = 0.98 # float .to_f
+person_has_car = true  #booling true or false
+none = nil #ничего
