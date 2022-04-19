9f2ebc3a002b943b55ec98aede4f34012d8cbb8a
Derkach Alexander <derkach2288@gmail.com>
2022-04-19 13:26:18 +0300

    commit1

---
+++ lesson2.rb
@@ -0,0 +1,12 @@
+words = "веб сайт it"
+word = "!"
+puts (words.upcase() + word)
+
+some_text = '     Много пробелов     '
+puts (some_text.strip())
+
+some_text = 'Много пробелов'
+puts (some_text.length())
+
+website = "itProger"
+puts (website.include? "it")
