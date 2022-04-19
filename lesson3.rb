58e04032a55eed3e5f800b9ecbf455ed7aa21c63
Derkach Alexander <derkach2288@gmail.com>
2022-04-19 13:42:45 +0300

    commit1

---
+++ lesson3.rb
@@ -0,0 +1,15 @@
+x = 5
+y = 10
+res = x + y
+res = x - y
+res = x * y
+res = 2 ** 3
+puts res
+res = y % x
+puts ("результат остатка от деления" + " " + res.to_s)
+
+num = -14.65
+puts (num.abs()) #по модулю
+puts (num.round()) #округление
+puts Math.sqrt(144)
+puts Math.log(1)
