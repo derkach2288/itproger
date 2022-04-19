1bab2c7a35e875cd6308f5c05d4f4e20bfa78c52
Derkach Alexander <derkach2288@gmail.com>
2022-04-19 13:11:15 +0300

    commit

--- lesson1.rb
+++ lesson1.rb
@@ -8,5 +8,5 @@ name = 'Привет всем' # string .to_s
 puts (name)
 
 happines = 0.98 # float .to_f
-person_has_car = true  #booling true/false
+person_has_car = true  #boolean true/false
 none = nil #ничего
