11eb960c4b1c3058676908c860fde9f565fb25ae
Derkach Alexander <derkach2288@gmail.com>
2022-04-21 18:47:14 +0300

    addet #

--- lesson2.rb
+++ lesson2.rb
@@ -1,12 +1,12 @@
 words = "веб сайт it"
 word = "!"
-puts (words.upcase() + word)
+puts (words.upcase() + word) # .upcase() - переводит текст в верхний регистр .downcase - в нижний регистр
 
 some_text = '     Много пробелов     '
-puts (some_text.strip())
+puts (some_text.strip())         # .strip() - удаляет пробелы
 
 some_text = 'Много пробелов'
-puts (some_text.length())
+puts (some_text.length())  # .length() считает количество символов
 
 website = "itProger"
-puts (website.include? "it")
+puts (website.include? "it")  # .include? - поиск, результат true/false
