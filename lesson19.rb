c6e9b0b6f8cc7cebbd570e75d7a5a29b47238ec1
Derkach Alexander <derkach2288@gmail.com>
2022-04-22 18:41:42 +0300

    addet initialize @

--- lesson19.rb
+++ lesson19.rb
@@ -1,20 +1,17 @@
 class Car  # название класса всегда с большой буквы
     attr_accessor :speed, :model, :color
 
-    def initialize(speed)
-        puts ("Hello " + speed.to_s)
+    def initialize(speed, model, color)
+        @speed = speed
+        @model = model
+        @color = color
     end
 end
 
-bmw = Car.new(230)
-bmw.speed = 230
-bmw.model = "X6"
-bmw.color = "Black"
+bmw = Car.new(230, "X6", "Black")
 
-audi = Car.new(250)
-audi.speed = 250
-audi.model = "Q7"
-audi.color = "White"
+audi = Car.new(250, "Q7", "White")
+
 
 puts bmw.speed
 puts audi.speed
