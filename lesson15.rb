b7252316d79a64d24a615dcb1975ffe6e34d606c
Derkach Alexander <derkach2288@gmail.com>
2022-04-22 12:27:10 +0300

    File

---
+++ lesson15.rb
@@ -0,0 +1,24 @@
+File.open("text/milpro.txt") do |x| # присваивает файлу переменну, например, х. Далее можем обращаться к файлу через переменную
+    puts x.read().include? "кг" # .read() - команда прочитать файл, далее можем дать команду, к примеру, .include? и что-то найти
+end
+
+File.open("text/milpro.txt") do |x| 
+    puts x.readline() # .readline() - читает строку. При каждой последующе команде .readline() - читает следующую строку
+    puts x.readline()
+    puts x.readline()
+end
+
+File.open("text/milpro.txt") do |x| 
+    puts x.readchar() # .readchar() - выводит один символ. 
+    puts x.readchar()
+    puts x.readchar()
+end
+
+
+
+File.open("text/milpro.txt") do |x| 
+        for y in x.readlines()   # .readlines() -Массив из всех строк
+            puts y # построчно прочитываем весь вайл
+        end
+end
+
