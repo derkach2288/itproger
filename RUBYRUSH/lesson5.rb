b7be59cb5f5c60946c522bd27d52aa92dd5afd2c
Derkach Alexander <derkach2288@gmail.com>
2022-04-26 18:16:37 +0300

    добавлено вероятность стать на ребро 1/10

--- RUBYRUSH/lesson5.rb
+++ RUBYRUSH/lesson5.rb
@@ -14,3 +14,12 @@ if x.sample == 0
     else
     puts "решка"
 end
+
+# Добавлено ребро, выпадает с вероятностью 1 к 10
+if rand(11) == 10
+    puts "Ребро"
+    elsif rand(0..1) == 0 
+    puts "Орел"
+    else
+    puts "решка"
+end
