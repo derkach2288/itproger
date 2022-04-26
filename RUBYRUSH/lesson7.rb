b626ae4e02b6db138652162378baea37dd29d722
Derkach Alexander <derkach2288@gmail.com>
2022-04-26 19:08:57 +0300

    сократил программу

--- RUBYRUSH/lesson7.rb
+++ RUBYRUSH/lesson7.rb
@@ -1,8 +1,5 @@
-x = Array.new
 puts "Введите 3 числа"
-x[0] = gets
-x[1] = gets
-x[2] = gets
+x = [gets, gets, gets]
 
 y = (x[0].to_i + x[1].to_i + x[2].to_i)/3
 puts "среднее арифметическое " + y.to_s
