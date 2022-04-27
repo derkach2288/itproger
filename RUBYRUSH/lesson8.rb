2d38e1599c86c1651070af7339ed7ea676cf97d2
Derkach Alexander <derkach2288@gmail.com>
2022-04-27 11:34:59 +0300

    добавлено выбор валют

--- RUBYRUSH/lesson8.rb
+++ RUBYRUSH/lesson8.rb
@@ -1,9 +1,21 @@
-puts "Какой сегодня курс гривны ?"
-curse = gets.to_f
+puts "Какая у вас валюта?
+    1. Доллары
+    2. Гривны"
+    
+    val = gets.chomp
 
-puts "сколько у вас долларов?"
-nal = gets.to_f
-
-usd = curse*nal
-
-puts " Итого = " + usd.to_s + "гривен"
+    if val == "1"
+        puts "Какой сегодня курс доллара ?"
+        curse = gets.to_f
+        puts "сколько у вас долларов?"
+        nal = gets.to_f
+        usd = curse*nal
+        puts " Итого = " + usd.to_s + "гривен"
+    else val == "2"
+        puts "Какой сегодня курс гривны ?"
+        curse = gets.to_f
+        puts "сколько у вас гривен?"
+        nal = gets.to_f
+        usd = (nal/curse).round(2)
+        puts " Итого = " + usd.to_s + "долларов"
+    end
