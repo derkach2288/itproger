94a55b2a2cb9e37214b70f7f06a0742dfada15dc
Derkach Alexander <derkach2288@gmail.com>
2022-04-27 14:22:49 +0300

    sleep

---
+++ RUBYRUSH/lesson10.rb
@@ -0,0 +1,15 @@
+puts "Start"
+sleep 1              # sleep - программа привыполнении засыпает на указанное количество секунд
+
+puts "1 km"
+sleep 5
+
+puts "2 km"
+sleep 1
+
+puts "3 km"
+sleep 1
+
+puts "Finish"
+
+gets
