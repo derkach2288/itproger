9ee1433dadecfe5ed77c8584962e08f77bec84e3
Derkach Alexander <derkach2288@gmail.com>
2022-05-02 15:33:33 +0300

    модернизация программы с помощью метода

---
+++ RUBYRUSH/lesson9a.rb
@@ -0,0 +1,30 @@
+
+def nume_y(x, y)
+
+    if y == x
+        abort "Поздравляем, вы выиграли"
+    elsif 
+        (y+1) == x or (y+2) == x
+        puts "тепло, нужно больше"
+    elsif
+        (y-1) == x or (y-2) == x
+     puts "тепло, нужно меньше"
+    else
+        puts "холодно"
+    end
+   
+end
+
+x = rand(16)
+puts "Загадано число от 0 до 15, включительно, отгадайте c 3х попыток"
+
+y = gets.to_i
+nume_y(x, y)
+
+y = gets.to_i
+nume_y(x, y)
+
+y = gets.to_i
+nume_y(x, y)
+
+puts "Вы проиграли. Загаданное число " + x.to_s
