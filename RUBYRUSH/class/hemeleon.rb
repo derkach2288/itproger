865a7a9e558ee17cf382a367c43344a30cddb44a
Derkach Alexander <derkach2288@gmail.com>
2022-05-06 17:24:27 +0300

    д/з класс «Хамелеон», у которого есть один метод поменять цвет

---
+++ RUBYRUSH/class/hemeleon.rb
@@ -0,0 +1,18 @@
+class Hameleon
+
+    def change_color (color)
+        
+        puts "Теперь я #{color}"
+    end
+   
+end
+
+
+
+
+hameleon = Hameleon.new
+
+hameleon.change_color("красный")
+hameleon.change_color("синий")
+hameleon.change_color("зеленый")
+
