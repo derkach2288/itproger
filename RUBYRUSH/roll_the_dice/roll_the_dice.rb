3053d03a83f23195f631d29ca4d2052e767dbea1
Derkach Alexander <derkach2288@gmail.com>
2022-05-15 12:36:30 +0300

    Добавлен эффект вращения

--- RUBYRUSH/roll_the_dice/roll_the_dice.rb
+++ RUBYRUSH/roll_the_dice/roll_the_dice.rb
@@ -1,7 +1,15 @@
+def show_rolling_die
+    10.times do
+        print "#{rand(1..6)}\r"
+        sleep 0.1
+    end
+end
+
 puts "Сколько кубиков кинуть?"
 
 number = gets.to_i
 
 number.times do 
+show_rolling_die
 puts rand(1..6)
 end
